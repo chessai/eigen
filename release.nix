3d61083c2ee44f96252efeb9eeabebb69fb2b3ed
chessai <chessai1996@gmail.com>
2019-06-21 14:02:02 -0400

    remove nix

--- release.nix
+++
@@ -1,5 +0,0 @@
-{
-    ghc802  = import ./default.nix { compiler = "ghc802";  };
-    ghc822  = import ./default.nix { compiler = "ghc822";  };
-    ghc842  = import ./default.nix { compiler = "ghc842";  };  
-}
